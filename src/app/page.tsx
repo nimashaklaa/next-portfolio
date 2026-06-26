409a328aa00a587449ecf710512dad970322ed5d
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-06-26 19:21:08 +0530

    feat: Add Navbar and Footer components to homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,19 @@ import HeroHeader from "@/app/components/HeroHeader";
 import HeroBio from "@/app/components/HeroBio";
 import HeroActions from "@/app/components/HeroActions";
 import ColourPallet from "@/app/components/ColorPallet";
+import Navbar from "@/app/components/navbar";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center bg-white font-sans dark:bg-black">
+      <Navbar />
       <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-6 py-16 sm:px-16 sm:py-32">
         <HeroHeader />
         <HeroBio />
         <HeroActions />
       </main>
+      <Footer />
       <ColourPallet />
     </div>
   );
